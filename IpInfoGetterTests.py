4f517354cb55635171381685b86bda2e328edff8
LD01202 <129019896+LD01202@users.noreply.github.com>
2023-10-25 19:29:26 +0800

    TestCases are here

---
+++ IpInfoGetterTests.py
@@ -0,0 +1,20 @@
+from IpInfoGetterv2 import case_1, case_2, case_3, case_4, case_5, inv_case
+import pytest
+
+def test_case1():
+    assert case_1() == True
+
+def test_case2():
+    assert case_2() == True
+
+def test_case3():
+    assert case_3() == True
+
+def test_case4():
+    assert case_4() == True
+
+def test_case5():
+    assert case_5() == True
+
+def test_defaultCase():
+    assert inv_case == True
